5ad01f541478612f8992f0a12f7f221e1ea47bd3
Tania Munoz <tania.m.jimenez24@gmail.com>
2018-09-16 15:54:38 -0500

    create board

--- Game_of_life.rb
+++ Game_of_life.rb
@@ -1,4 +1,30 @@
 class Game_Life
+	@@board_size = 10
+	def print_board
+		@@board.each do |column|
+         column.each do |card|
+          print card
+          print '  ' 
+         end
+        puts
+        
+      end
+	end
 
-	
+	def  empty_board
+      @@board = []
+      # llenar tablero
+      @@board_size.times do
+        column = []
+        @@board_size.times do
+          column.push '0'
+        end
+        @@board.push column
+     end
+	end
+
+	def start
+		empty_board
+		print_board
+	end
 end
---
+++ main.rb
@@ -0,0 +1,4 @@
+require_relative 'Game_of_life'
+game = Game_Life.new
+    game.start
+
